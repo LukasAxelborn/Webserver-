67ad82ae9e2ef97bb08a1046bc2a789a347c53a8
Lukas Axelborn <Lukasaxelborn@live.se>
2021-02-10 15:12:22 +0100

    start på server

---
+++ serverUDP.c
@@ -0,0 +1,43 @@
+#include <stdio.h> /* This is the server code*/
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define SERVER_PORT 12345 /* arbitary, but client & server must agree, std is 80, though it may req super-user */
+#define BUF_SIZE 4096     /* block transfer size */
+#define QUEUE_SIZE 10
+
+int main(int argc, char *argv[])
+{
+    int s, b, l, sa, fd, bytes, on = 1;
+    char buf[BUF_SIZE], tmp[BUF_SIZE]; /* buffer for outging file */
+    struct sockaddr_in channel;        /* holds IP address */
+
+    /* Build address structure to bind to socket */
+    memset(&channel, 0, sizeof(channel));
+    channel.sin_family = AF_INET;
+    channel.sin_addr.s_addr = htonl(INADDR_ANY);
+    channel.sin_port = htons(SERVER_PORT);
+
+    /* Passive open. Wait for connection */
+
+    /* create socket */
+    s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (s < 0)
+        fatal("socket failed");
+    setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof(on));
+
+    /* bind and listen */
+    b = bind(s, (struct sockaddr *)&channel, sizeof(channel));
+    if (b < 0)
+        fatal("bind failed");
+
+    l = listen(s, QUEUE_SIZE); //specify queue size
+    if (l < 0)
+        fatal("listen failed");
+}
