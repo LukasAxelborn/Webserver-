ebac0fe8b8dfa9ccff67391f5abdf674d24d6eab
LukasAxelborn <Lukasaxelborn@live.se>
2021-02-11 14:23:39 +0100

    allt funkar nästan

--- clientUDP.c
+++ clientUDP.c
@@ -13,7 +13,7 @@
 
 
 #define SERVER_PORT 12345 /* arbitary, but client & server must agree, std is 80, though it may req super-user */
-#define BUF_SIZE 1024     /* block transfer size */
+//#define BUF_SIZE 1024     /* block transfer size */
 
 int main(int argc, char **argv)
 {
@@ -21,7 +21,8 @@ int main(int argc, char **argv)
 
     int sockfd;
     struct sockaddr_in serverAddr;
-    char buffer [BUF_SIZE];
+    //char buffer [BUF_SIZE];
+    uint32_t numbers;
     socklen_t addr_size;
 
     sockfd = socket(PF_INET, SOCK_DGRAM, 0);
@@ -31,14 +32,15 @@ int main(int argc, char **argv)
     serverAddr.sin_port = htons(SERVER_PORT);
     inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
 
-    strcpy(buffer, "");
-    sendto(sockfd, buffer, BUF_SIZE, 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
-    printf("[+]Data send: %s\n", buffer);
+    sendto(sockfd, NULL, 0, 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+    printf("[+]Data send: %s\n", "nothing");
 
     addr_size = sizeof(serverAddr);
 
-    recvfrom(sockfd, buffer, BUF_SIZE, 0, (struct sockaddr*)&serverAddr, &addr_size);
-    time_t servertime = (long)atoi(buffer);
+    recvfrom(sockfd, &numbers, sizeof(uint32_t), 0, (struct sockaddr*)&serverAddr, &addr_size);
+    
+    time_t servertime = ntohs(numbers);
+    
     printf("[+]Data received: %s\n", ctime(&servertime));
 
     close(sockfd);
